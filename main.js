36ad8db30e17079ba8cf2c1003b4264c9f121209
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-07-04 05:30:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var carSpecs = ["Civic", 1.8];
+//console.log(carSpecs);
+var carOwner = ["Ali", 29];
+console.log("car specs are ".concat(carSpecs, " and owner name is ").concat(carOwner[0], " he is ").concat(carOwner[1], " years old."));
---
+++ main.ts
@@ -0,0 +1,9 @@
+//tuples
+type car=[string, number]
+let carSpecs:car= ["Civic", 1.8]
+//console.log(carSpecs);
+
+
+let carOwner: [string, number]= ["Ali", 29];
+
+console.log(`car specs are ${carSpecs} and owner name is ${carOwner[0]} he is ${carOwner[1]} years old.`);
